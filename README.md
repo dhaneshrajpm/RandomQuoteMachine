a2fdd9e7555805431ca1d1f8a3e2ee7127d3ad6b
dhaneshrajpm <dhaneshraj2311@gmail.com>
2022-03-20 20:35:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ejeblk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ejeblk)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div id="wrapper">
+      <div id="quote-box">
+        <div id="text"></div>
+        <div id="author"></div>
+        <div id="new-quote"></div>
+        <a id="tweet-quote"></a>
+      </div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
